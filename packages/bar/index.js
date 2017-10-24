74a2898f4d11ad16fe58b2bcaf3e2d81338acb33
Sean Matheson <sean@ctrlplusb.com>
2017-10-24 15:33:39 +0100

    Sets up a 'proper' release of foo and bar onto npm along with a bin for foo.

--- packages/bar/index.js
+++ packages/bar/index.js
@@ -1,4 +1,4 @@
 const chalk = require("chalk");
-const foo = require("foo");
+const foo = require("@ctrlplusb/yarn-workspace-root-bug-foo");
 
 module.exports = `I am ${chalk.blueBright("bar")}, I am not ${foo}`;
